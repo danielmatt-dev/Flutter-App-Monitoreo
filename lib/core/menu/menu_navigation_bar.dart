e6af179a628c3d6abaedc8c13f089e7a8ecfd66c
Daniel <danielmatt1701@gmail.com>
2024-06-01 15:24:25 -0600

    Implementación del menu de navegacion

---
+++ lib/core/menu/menu_navigation_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+// <>
+class MenuNavigationBar extends StatefulWidget {
+
+  const MenuNavigationBar({super.key});
+
+  @override
+  State<MenuNavigationBar> createState() => _MenuNavigationState();
+
+}
+
+class _MenuNavigationState extends State<MenuNavigationBar> {
+
+  int _index = 0;
+
+  final screens = [
+    const Center(child: Text('Home'),),
+    const Center(child: Text('Monitoreo'),),
+    const Center(child: Text('Descargas'),),
+    const Center(child: Text('Perfil'),),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: screens[_index],
+      bottomNavigationBar: NavigationBar(
+        height: height * 0.10,
+        selectedIndex: _index,
+        //labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        animationDuration: const Duration(seconds: 1),
+        onDestinationSelected: (index) => setState(() => _index = index),
+        destinations: const [
+          NavigationDestination(
+              icon: Icon( Icons.home_outlined ),
+              selectedIcon: Icon( Icons.home ),
+              label: 'Inicio'
+          ),
+          NavigationDestination(
+              icon: Icon(Icons.calendar_month_outlined),
+              selectedIcon: Icon(Icons.calendar_month),
+              label: 'Monitoreo'
+          ),
+          NavigationDestination(
+              icon: Icon(Icons.download_outlined),
+              selectedIcon: Icon(Icons.download),
+              label: 'Descargas'
+          ),
+          NavigationDestination(
+              icon: Icon(Icons.person_outlined),
+              selectedIcon: Icon(Icons.person),
+              label: 'Perfil'
+          ),
+        ],
+      ),
+    );
+  }
+
+}
