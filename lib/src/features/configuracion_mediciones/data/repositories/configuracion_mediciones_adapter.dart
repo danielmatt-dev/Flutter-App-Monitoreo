c97e01d4c1af42f733305dd88e2dd8be6b3e4b9d
Daniel <danielmatt1701@gmail.com>
2024-06-09 13:57:35 -0600

    Implementación Auth Repository en Configuracion mediciones

--- lib/src/features/configuracion_mediciones/data/repositories/configuracion_mediciones_adapter.dart
+++ lib/src/features/configuracion_mediciones/data/repositories/configuracion_mediciones_adapter.dart
@@ -3,45 +3,41 @@ import 'package:app_plataforma/src/features/configuracion_mediciones/data/data_s
 import 'package:app_plataforma/src/features/configuracion_mediciones/data/models/mapper/configuracion_mediciones_model_mapper.dart';
 import 'package:app_plataforma/src/features/configuracion_mediciones/domain/entities/configuracion_mediciones.dart';
 import 'package:app_plataforma/src/features/configuracion_mediciones/domain/repositories/configuracion_mediciones_repository.dart';
-import 'package:app_plataforma/src/features/valor_glucosa/data/data_sources/local/valor_glucosa_local_datasource.dart';
+import 'package:app_plataforma/src/features/paciente/domain/repositories/auth_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class ConfiguracionMedicionesAdapter extends ConfiguracionMedicionesRepository {
 
   final ConfiguracionRemoteDatasource remote;
   final ConfiguracionLocalDatasource local;
-  final ValorGlucosaLocalDatasource valorGlucosaLocalDatasource;
+  final AuthRepository repository;
   final ConfiguracionMedicionesModelMapper mapper;
 
   ConfiguracionMedicionesAdapter({
     required this.remote,
     required this.local,
-    required this.valorGlucosaLocalDatasource,
+    required this.repository,
     required this.mapper
   });
 
   @override
   Future<Either<Exception, ConfiguracionMediciones>> buscarMedicionesDelDia() async {
 
-    final response = await remote.buscarConfiguracion(valorGlucosaLocalDatasource.getIdPaciente());
-
-    return response.fold(
+    return repository.getIdPaciente().fold(
             (failure) => Left(failure),
-            (model) async {
+            (idPaciente) async {
 
-              final success = await local.clearTable();
+              final response = await remote.buscarConfiguracion(idPaciente);
 
-              if(success.isLeft()){
-                return Left(Exception('Error al eliminar los registros de la tabla'));
-              }
+              return response.fold(
+                      (failure) => Left(failure),
+                      (model) async {
 
-              final result = await local.saveConfiguracion(model);
-
-              if(result.isLeft()){
-                return Left(Exception('Error al guardar las mediciones del paciente'));
-              }
-
-              return Right(mapper.toConfiguracionMediciones(model));
+                        await local.clearTable();
+                        await local.saveConfiguracion(model);
+                        return Right(mapper.toConfiguracionMediciones(model));
+                      }
+              );
 
             }
     );
