f467a74493a694592e531ea00ebd1a6dd4f7ef08
Daniel <danielmatt1701@gmail.com>
2024-07-10 07:09:00 -0600

    Mejoras card timeline

--- lib/src/features/valor_response/presentation/widgets/card_timeline.dart
+++ lib/src/features/valor_response/presentation/widgets/card_timeline.dart
@@ -1,6 +1,6 @@
 import 'package:app_plataforma/src/core/styles/app_text_styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 class CardTimeline extends StatelessWidget {
 
@@ -23,45 +23,52 @@ class CardTimeline extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
     final height = MediaQuery.of(context).size.height;
 
-    return ListTile(
-      leading: SizedBox(
-        width: height*0.09,
-        child: Center(
-          child: AppTextStyles.autoBodyStyle(
-              text: hora,
-              color: colorScheme.primary,
-              maxLines: 1,
-              height: height,
-              percent: 0.01
-          ),
-        ),
-      ),
-      title: Row(
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0, top: 8.0, left: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            width: 5,
-            height: 50,
-            color: color,
-            margin: const EdgeInsets.only(right: 10),
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: AppTextStyles.autoBodyStyle(
+                text: hora,
+                color: colorScheme.primary,
+                maxLines: 1,
+                height: height,
+                percent: 0.022,
+              ),
+            ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppTextStyles.autoBodyStyle(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Container(
+              width: 5,
+              height: 50,
+              color: color,
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppTextStyles.autoBodyStyle(
                   text: titulo,
                   color: colorScheme.primary,
                   maxLines: 1,
-                  height: height
-              ),
-              AppTextStyles.autoBodyStyle(
+                  height: height,
+                ),
+                AppTextStyles.autoBodyStyle(
                   text: subtitulo,
                   color: colorScheme.primary,
                   maxLines: 1,
                   height: height,
-                  percent: 0.02
-              )
-            ],
-          )
+                  percent: 0.02,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
