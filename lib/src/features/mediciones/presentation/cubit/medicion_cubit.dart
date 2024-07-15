6ca022b5498806a1b463c1dec426391dd8eb48f8
Daniel <danielmatt1701@gmail.com>
2024-07-14 21:14:13 -0600

    Medicion Cubit implementado

---
+++ lib/src/features/mediciones/presentation/cubit/medicion_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:app_plataforma/src/features/mediciones/domain/entities/medicion.dart';
+import 'package:app_plataforma/src/features/mediciones/domain/usecases/buscar_mediciones_del_dia.dart';
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'medicion_state.dart';
+part 'medicion_cubit.freezed.dart';
+
+class MedicionCubit extends Cubit<MedicionState>{
+
+  final BuscarMedicionesDelDia buscarMediciones;
+
+  MedicionCubit({
+    required this.buscarMediciones
+  }): super(const MedicionState.initial());
+
+  Future<void> buscarMedicion(String tipo) async {
+
+    emit(const MedicionState.initial());
+
+    final result = await buscarMediciones.call(tipo);
+
+    result.fold(
+            (failure) => emit(MedicionState.error(failure.toString())),
+            (mediciones) => emit(MedicionState.listSuccess(mediciones))
+    );
+
+  }
+
+
+}
+
+// <>
---
+++ lib/src/features/mediciones/presentation/cubit/medicion_state.dart
@@ -0,0 +1,11 @@
+part of 'medicion_cubit.dart';
+
+@freezed
+sealed class MedicionState with _$MedicionState {
+
+  const factory MedicionState.initial() = _Initial;
+  const factory MedicionState.listSuccess(List<Medicion> mediciones) = _Success;
+  const factory MedicionState.emptyList() = _Empty;
+  const factory MedicionState.error(String message) = _Error;
+
+}
