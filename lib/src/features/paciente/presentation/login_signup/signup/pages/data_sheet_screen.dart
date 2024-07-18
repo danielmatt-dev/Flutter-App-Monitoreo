9bb0ae55a2f3feb6ab8ff5c6be8dfb633ada2f6d
Daniel <danielmatt1701@gmail.com>
2024-07-18 06:40:20 -0600

    Nuevo widget date bottom picker implementado

--- lib/src/features/paciente/presentation/login_signup/signup/pages/data_sheet_screen.dart
+++ lib/src/features/paciente/presentation/login_signup/signup/pages/data_sheet_screen.dart
@@ -1,6 +1,9 @@
 import 'package:app_plataforma/src/core/styles/app_size_box_styles.dart';
+import 'package:app_plataforma/src/core/styles/app_text_styles.dart';
 import 'package:app_plataforma/src/features/paciente/presentation/login_signup/signup/widgets/info_section.dart';
 import 'package:app_plataforma/src/shared/widgets/fast_text_field_custom.dart';
+import 'package:bottom_picker/bottom_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,6 +12,7 @@ class DataSheetScreen extends StatefulWidget {
 
   @override
   State<DataSheetScreen> createState() => _DataSheetScreenState();
+
 }
 
 class _DataSheetScreenState extends State<DataSheetScreen> {
@@ -39,25 +43,47 @@ class _DataSheetScreenState extends State<DataSheetScreen> {
   @override
   Widget build(BuildContext context) {
 
+    final colorScheme = Theme.of(context).colorScheme;
     final height = MediaQuery.of(context).size.height;
 
     Future<void> selectedDate() async {
-      dateSelected = await showDatePicker(
-          context: context,
-          locale: const Locale('es', 'ES'),
-          initialDate: dateSelected,
-          firstDate: DateTime(1900),
-          lastDate: DateTime(2500)
-      );
-
-      if(dateSelected != null) {
-        setState(() {
-          print(dateSelected);
-          _nacimientoController.text =  DateFormat('d \'de\' MMMM \'del\' yyyy', 'es').format(dateSelected!);
-          print(_nacimientoController.text);
-        });
-      }
-
+      BottomPicker.date(
+        pickerTitle: AppTextStyles.autoBodyStyle(
+            text: 'Selecciona una fecha',
+            color: colorScheme.onBackground,
+            height: height
+        ),
+        displayCloseIcon: false,
+        buttonWidth: 200,
+        initialDateTime: dateSelected ?? DateTime.now(),
+        minDateTime: DateTime(1900),
+        maxDateTime: DateTime(2500),
+        onSubmit: (date) {
+          setState(() {
+            dateSelected = date;
+            _nacimientoController.text = DateFormat('d \'de\' MMMM \'del\' yyyy', 'es').format(date);
+          });
+        },
+        backgroundColor: colorScheme.background,
+        dateOrder:  DatePickerDateOrder.dmy,
+        pickerTextStyle: AppTextStyles.buttonStyle(
+            color: colorScheme.onBackground,
+            size: height*0.022
+        ),
+        buttonStyle: BoxDecoration(
+          color: colorScheme.onBackground,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        buttonPadding: 5,
+        buttonContent: AppTextStyles.autoBodyStyle(
+            text: 'Guardar',
+            color: colorScheme.background,
+            height: height,
+            percent: 0.022,
+            textAlign: TextAlign.center
+        ),
+        dismissable: true,
+      ).show(context);
     }
 
     return SingleChildScrollView(
