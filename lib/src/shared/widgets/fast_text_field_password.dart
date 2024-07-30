0356cbdc4af74eef14f89e8c8b71b4843cf85cff
Daniel <danielmatt1701@gmail.com>
2024-07-30 00:58:38 -0600

    Mejoras en fast text field password

--- lib/src/shared/widgets/fast_text_field_password.dart
+++ lib/src/shared/widgets/fast_text_field_password.dart
@@ -6,29 +6,45 @@ import 'package:flutter_fast_forms/flutter_fast_forms.dart';
 
 class FastTextFieldPassword extends StatelessWidget {
   final ValueChanged<String> onChanged;
+  final String hintText;
+  final double hintOpacity;
   final String labelText;
+  final Color? hintColor;
   final bool isInvalid;
   final String errorText;
   final VoidCallback toggleVisibility;
   final bool obscureText;
   final bool enabled;
+  final Color? backgroundColor;
 
   const FastTextFieldPassword({
     super.key,
     required this.onChanged,
+    this.hintText = '',
+    this.hintOpacity = 0.4,
+    this.hintColor,
     required this.labelText,
     required this.isInvalid,
     required this.errorText,
     required this.toggleVisibility,
     required this.obscureText,
-    this.enabled = true
+    this.enabled = true,
+    this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
+
     final colorScheme = Theme.of(context).colorScheme;
     final brightness = Theme.of(context).brightness;
     final height = MediaQuery.of(context).size.height;
+
+    OutlineInputBorder buildBorder(Color color) {
+      return OutlineInputBorder(
+        borderSide: BorderSide(color: color),
+        borderRadius: BorderRadius.circular(8),
+      );
+    }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,52 +55,41 @@ class FastTextFieldPassword extends StatelessWidget {
             height: height
         ),
         AppSizeBoxStyle.sizeBox(height: height, percentage: 0.01),
-        SizedBox(
-          height: height * 0.09,
-          width: double.infinity,
-          child: FastTextField(
-            name: labelText,
-            obscureText: obscureText,
-            onChanged: (value) {
-              onChanged(value ?? '');
-            },
-            inputFormatters: [FilteringTextInputFormatter.deny(RegExp(r'\s'))],
-            decoration: InputDecoration(
-              hintText: labelText,
-              hintStyle: AppTextStyles.bodyStyle(color: colorScheme.onBackground.withOpacity(0.4), size: height * 0.022),
-              labelText: null,
-              errorStyle: AppTextStyles.bodyStyle(color: colorScheme.error, size: height * 0.015),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: isInvalid ? colorScheme.error : colorScheme.onBackground.withOpacity(0.2),
-                ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: isInvalid ? colorScheme.error : colorScheme.onBackground,
-                ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: colorScheme.error),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
-                onPressed: toggleVisibility,
-              ),
-              filled: true,
-              fillColor: enabled
-                  ? brightness == Brightness.light ? Colors.white : Colors.black38
-                  : null,
+        FastTextField(
+          name: labelText,
+          obscureText: obscureText,
+          onChanged: (value) {
+            onChanged(value ?? '');
+          },
+          inputFormatters: [FilteringTextInputFormatter.deny(RegExp(r'\s'))],
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: AppTextStyles.bodyStyle(
+              color: hintColor ?? colorScheme.onBackground.withOpacity(hintOpacity),
+              size: height * 0.022,
             ),
-            style: TextStyle(
-              color: isInvalid ? colorScheme.error : colorScheme.onBackground,
-              fontSize: height * 0.025,
+            errorText: isInvalid ? errorText : null,
+            errorStyle: AppTextStyles.bodyStyle(color: colorScheme.error, size: height * 0.015),
+            enabledBorder: buildBorder(isInvalid ? colorScheme.error : colorScheme.onBackground.withOpacity(0.2)),
+            focusedErrorBorder: buildBorder(colorScheme.error),
+            focusedBorder: buildBorder(isInvalid ? colorScheme.error : colorScheme.onBackground),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: colorScheme.error),
+              borderRadius: BorderRadius.circular(8),
             ),
-            textInputAction: TextInputAction.next,
+            suffixIcon: IconButton(
+              icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
+              onPressed: toggleVisibility,
+            ),
+            filled: true,
+            fillColor: backgroundColor ?? (brightness == Brightness.light ? Colors.white : Colors.black38),
           ),
+          style: TextStyle(
+            color: isInvalid ? colorScheme.error : colorScheme.onBackground,
+            fontSize: height * 0.025,
+            fontWeight: FontWeight.w500,
+          ),
+          textInputAction: TextInputAction.next,
         ),
       ],
     );
